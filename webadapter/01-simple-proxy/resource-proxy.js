e9fbdf0228fce337a291c15882bd327700c95837
Yogesh Dhanapal <geoyogesh@gmail.com>
2015-08-08 15:13:22 -0400

    embedding authorization token

--- webadapter/01-simple-proxy/resource-proxy.js
+++ webadapter/01-simple-proxy/resource-proxy.js
@@ -2,28 +2,36 @@
 
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 var request = require('request');
 var utils = require("./modules/utils.js");
 
 //let destinationUrl = 'http://localhost:1337';
 var destinationUrl = 'http://sampleserver1.arcgisonline.com';
-var isAuthenticated = 'true';
-var tokens;
+var isAuthenticated = true;
+var token;
 
 
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-    tokens = utils.getArcGISServerToken();
-    console.log(tokens);
     var urlp = url.parse(req.url);
+    var resourceUrl = url.parse(urlp.query);
     if (urlp.pathname.toLowerCase() === '/proxy.js') {
-        var reqUrl = urlp.query;
-        console.log(req.url);
+        if (isAuthenticated==true)
+        {
+            if (token===undefined) {
+                token = utils.getArcGISServerToken();
+                console.log(token);
+            }
+            var qs= querystring.parse(resourceUrl.query);
+            qs['token'] = token;
+            resourceUrl = url.parse(url.resolve(resourceUrl.href,'?'+querystring.stringify(qs)));
+        }
+
         var options = {
             headers: req.headers,
-            url: reqUrl
+            url: resourceUrl
         };
-
         var destinationResponse = req.pipe(request(options));
         destinationResponse.pipe(res);
     }
@@ -32,3 +40,10 @@ http.createServer(function (req, res) {
         res.end('invalid path');
     }
 }).listen(port);
+
+/*
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(req.url);
+}).listen(1338);
+*/
---
+++ webadapter/01-simple-proxy/temp/urlediting.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var request = require('request');
+
+//let destinationUrl = 'http://localhost:1338';
+var destinationUrl = 'http://sampleserver1.arcgisonline.com';
+
+console.log(destinationUrl);
